fdc32a197d51e17955cfedfb7a583352682c2f2c
Caro <grossocrln@gmail.com>
2022-11-18 14:28:31 -0300

    deploy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ContactoComponent } from './components/contacto/contacto.component';
+import { HomeComponent } from './components/home/home.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'trabajos', component: TrabajosComponent},
+  {path: 'contacto', component: ContactoComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ServiciosComponent } from './components/servicios/servicios.component';
+import { TrabajosComponent } from './components/trabajos/trabajos.component';
+import { PrincipalComponent } from './components/principal/principal.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    FooterComponent,
+    ServiciosComponent,
+    TrabajosComponent,
+    PrincipalComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule,
